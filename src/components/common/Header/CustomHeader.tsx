a679dcbe092a2164879a53aa25a7d7f39d99f158
seon022 <seon022@icloud.com>
2024-11-25 14:25:10 +0900

    ✨ Feat :  커스텀 헤더 컴포넌트 추가 - 뒤로가기 버튼 - 제목, 추가버튼 props - Ref: #QTVY-93

---
+++ src/components/common/Header/CustomHeader.tsx
@@ -0,0 +1,53 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import styles from './CustomHeader.module.css';
+
+interface CustomHeaderProps {
+  title: string;
+  rightButton?: {
+    icon: string;
+    alt: string;
+    onClick?: () => void;
+  };
+}
+
+export const CustomHeader = ({ title, rightButton }: CustomHeaderProps) => {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  return (
+    <header className={styles.customHeader}>
+      <div className={styles.headerContent}>
+        <button
+          className={styles.backButton}
+          onClick={handleGoBack}>
+          <Image
+            src="/icons/left-arrow.png"
+            alt="뒤로가기"
+            width={24}
+            height={24}
+          />
+        </button>
+
+        <h1 className={styles.headerTitle}>{title}</h1>
+
+        {rightButton && (
+          <button
+            className={styles.rightButton}
+            onClick={rightButton.onClick}>
+            <Image
+              src={rightButton.icon}
+              alt={rightButton.alt}
+              width={24}
+              height={24}
+            />
+          </button>
+        )}
+      </div>
+    </header>
+  );
+};
