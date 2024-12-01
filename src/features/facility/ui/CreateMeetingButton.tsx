7ff1bed43b5a4bbc80e521354210cd316a70a26d
sssssubin <bstnqls@naver.com>
2024-12-02 00:55:45 +0900

    🐛 Fix: develop 병합으로 인한 시설 쪽 에러 처리 및 UI 수정

--- src/features/facility/ui/CreateMeetingButton.tsx
+++ src/features/facility/ui/CreateMeetingButton.tsx
@@ -7,7 +7,7 @@ export const CreateMeetingButton = ({
 }: CreateMeetingButtonProps) => {
   return (
     <div
-      className={`fixed bottom-[80px] left-0 w-full bg-white pl-[1.5rem] pr-[1.5rem] ${className}`}>
+      className={`sticky bottom-[80px] mx-auto w-full max-w-[600px] bg-white pl-[1.5rem] pr-[1.5rem] ${className}`}>
       <button className="w-full rounded-lg bg-blue-500 py-3 font-bold text-white">
         모임 생성하기
       </button>
