bd2532054f45d2b5c8f97c06e950969285f9c00f
seon022 <seon022@icloud.com>
2024-12-03 12:17:14 +0900

    🐛 Fix :  사용자 정보 조회 응답 스키마 반영 후 api 수정 - QTVY-121

--- src/mocks/data/userData.ts
+++ src/mocks/data/userData.ts
@@ -1,16 +1,22 @@
-// src/mocks/data/userData.ts
-
-import { UserType } from '@/entities/User';
 import { faker } from '@faker-js/faker';
 
 // 모의 사용자 데이터
-export const mockUsers: UserType[] = [
+export interface MockUser {
+  userId: string;
+  email: string;
+  password: string;
+  nickname: string;
+  profile_image?: string | null;
+  description?: string | null;
+  role: 'USER' | 'ADMIN';
+}
+export const mockUsers: MockUser[] = [
   {
     userId: faker.string.uuid(),
     email: 'test@example.com',
     password: 'password123',
     nickname: '운동하는개발자',
-    profilePath: faker.image.avatarGitHub(),
+    profile_image: faker.image.avatarGitHub(),
     description: '건강한 삶을 추구하는 개발자입니다.',
     role: 'USER'
   }
