972c3e61e9bbeed49d87eb524fe19b5b16d60fe8
sssssubin <bstnqls@naver.com>
2024-12-04 11:33:24 +0900

    🐛 Fix: 타입과 일반 import를 통합

--- src/components/common/Header/CustomHeader.tsx
+++ src/components/common/Header/CustomHeader.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Image from 'next/image';
 import { useRouter, usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
 
 interface CustomHeaderProps {
   title?: string;
