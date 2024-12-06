db1f94069ec431a1536bf9aba2b0111828e220ac
seon022 <seon022@icloud.com>
2024-12-06 11:46:15 +0900

    ✨ Feat :  채팅방 생성, 목록불러오기 작업중 - #QTVY-145

--- src/features/chat/types.ts
+++ src/features/chat/types.ts
@@ -1,11 +1,8 @@
 export interface ChatRoom {
   id: string;
   name: string;
-  latestMessage: string;
   latestTimestamp: string;
   type: 'group' | 'personal';
-  sports: string;
-  status: 'active' | 'completed';
 }
 
 export interface SideModalProps {
